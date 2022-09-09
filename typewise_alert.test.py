d1167e4025ad85bf0d97cabe98ec38a4544c7eec
manorama02 <53304326+manorama02@users.noreply.github.com>
2022-09-09 14:53:08 +0530

    Update typewise_alert.test.py

--- typewise_alert.test.py
+++ typewise_alert.test.py
@@ -7,6 +7,8 @@ class TypewiseTest(unittest.TestCase):
     self.assertTrue(typewise_alert.infer_breach(20, 50, 100) == 'TOO_LOW')
   def test_infer_breach_for_too_high(self):
     self.assertTrue(typewise_alert.infer_breach(200, 50, 100) == 'TOO_HIGH')
+  def bluff_test(self):
+    typewise_alert.check_and_alert('TO_CONTROLLER','PASSIVE_COOLING', 20)
 
 
 if __name__ == '__main__':
