8c77ff2a58380a6493771d335b796b1ec93072e8
manorama02 <53304326+manorama02@users.noreply.github.com>
2022-09-09 15:57:21 +0530

    Create string_calculator.test.py

---
+++ string_calculator.test.py
@@ -0,0 +1,11 @@
+import unittest
+
+class String_calculatorTest(unittest.TestCase):
+  def given_empty_string_zero_expected(self):
+    self.assertTrue(add("0") == 0)
+  
+
+
+
+if __name__ == '__main__':
+  unittest.main()
