7c758aa2855e35004788b130ce2e50b3792a161b
manorama02 <53304326+manorama02@users.noreply.github.com>
2022-09-09 14:12:28 +0530

    Update typewise_alert.py

--- typewise_alert.py
+++ typewise_alert.py
@@ -1,7 +1,7 @@
 
 def infer_breach(value, lowerLimit, upperLimit):
   if value < lowerLimit:
-    return 'TOO_LOW'
+    return 'TOO_LOW '
   if value > upperLimit:
     return 'TOO_HIGH'
   return 'NORMAL'
