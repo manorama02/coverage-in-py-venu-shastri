2fc7be328b702787e937fe85c1a569cec87c431b
manorama02 <53304326+manorama02@users.noreply.github.com>
2022-09-12 09:51:22 +0530

    Update string_calculator.test.py

--- string_calculator.test.py
+++ string_calculator.test.py
@@ -1,19 +1,11 @@
 import unittest
 import string_calculator
-
 class String_calculatorTest(unittest.TestCase):
-  def given_empty_string_zero_expected(self):
-	self.assertTrue(string_calculator.add("") == 0)
-    
-  def given_single_value_same_expected(self):
-	self.assertTrue(string_calculator.add("2") == 2)
-
-  def test_given_two_values_addition_expected(self):
-	self.assertTrue(string_calculator.add("2,3") == 5)
-
-  
-
-
-
+	def given_empty_string_zero_expected(self):
+		self.assertTrue(string_calculator.add("") == 0)
+	def given_single_value_same_expected(self):
+		self.assertTrue(string_calculator.add("2") == 2)
+	def test_given_two_values_addition_expected(self):
+		self.assertTrue(string_calculator.add("2,3") == 5)
 if __name__ == '__main__':
   unittest.main()
