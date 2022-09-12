ed301056cbc1f14f769d5a5e9188dd54d924d06c
manorama02 <53304326+manorama02@users.noreply.github.com>
2022-09-12 11:25:47 +0530

    coverage increased from 36 to 46 % for normal temp

--- typewise_alert.test.py
+++ typewise_alert.test.py
@@ -8,7 +8,8 @@ class TypewiseTest(unittest.TestCase):
   def test_infer_breach_for_too_high(self):
     self.assertTrue(typewise_alert.infer_breach(200, 50, 100) == 'TOO_HIGH')
   def test_infer_breach_for_normal(self):
-    self.assertTrue(typewise_alert.infer_breach(80, 50, 100) == 'NORMAL')
+    self.assertTrue(typewise_alert.infer_breach(80, 50, 100) == 'NORMAL')#increased coverage from 36 to 41%
+   
  
 
 if __name__ == '__main__':
