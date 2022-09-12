a7731eec5ab4435de742a051d17842b2babbe9b6
manorama02 <53304326+manorama02@users.noreply.github.com>
2022-09-12 10:59:01 +0530

    Update creditcard_validator.test.py

--- creditcard_validator.test.py
+++ creditcard_validator.test.py
@@ -6,16 +6,16 @@ class CreditcardValidatorTests(unittest.TestCase):
 		self.assertFalse(creditcard_validator.is_valid(""))
 	
 	def given_CCnumber_less_than_16_expected_false(self):
-		self.assertFalse(credit_card_validator.is_valid("12345"))
+		self.assertFalse(creditcard_validator.is_valid("12345"))
 
 	def given_CCnumber_more_than_16_expected_false(self):
-		self.assertFalse(credit_card_validator.is_valid("123456789012345678"))
+		self.assertFalse(creditcard_validator.is_valid("123456789012345678"))
 	
 	def given_incorrect_CCnumber_expected_false(self):
-		self.assertFalse(credit_card_validator.is_valid("4012888888881889"))
+		self.assertFalse(creditcard_validator.is_valid("4012888888881889"))
 
 	def given_correct_CCnumber_expected_true(self):
-		self.assertTrue(credit_card_validator.is_valid("4012888888881881"))
+		self.assertTrue(creditcard_validator.is_valid("4012888888881881"))
 
 if __name__ == "__main__":
 	unittest.main()
