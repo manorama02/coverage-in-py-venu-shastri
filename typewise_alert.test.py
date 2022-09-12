d6ade1d35fc1428a6c724401a4d1303d170a2d6c
manorama02 <53304326+manorama02@users.noreply.github.com>
2022-09-12 11:44:28 +0530

    passive cooling too high test

--- typewise_alert.test.py
+++ typewise_alert.test.py
@@ -12,8 +12,11 @@ class TypewiseTest(unittest.TestCase):
   def test_infer_breach_for_normal(self):
     self.assertTrue(typewise_alert.infer_breach(80, 50, 100) == 'NORMAL')#increased coverage from 36 to 41%
     
-  def test_classify_temperature(self):
-     self.assertTrue(typewise_alert.classify_temperature_breach('PASSIVE_COOLING', -2)== 'TOO_LOW')
+  def test_classify_temperature_passive_cooling_too_low(self):
+     self.assertTrue(typewise_alert.classify_temperature_breach('PASSIVE_COOLING', -2)== 'TOO_LOW')#increased coverage from 52 to 59%
+      
+  def test_classify_temperature_passive_cooling_too_high(self):
+    self.assertTrue(typewise_alert.classify_temperature_breach('PASSIVE_COOLING', 36)== 'TOO_HIGH')#increased coverage from 52 to 59%
     
    
  
