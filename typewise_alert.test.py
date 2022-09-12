14aea3316b735816e3466bc3b2642f4369aeb698
manorama02 <53304326+manorama02@users.noreply.github.com>
2022-09-12 11:39:29 +0530

    Passive cooling test for too low

--- typewise_alert.test.py
+++ typewise_alert.test.py
@@ -5,10 +5,16 @@ import typewise_alert
 class TypewiseTest(unittest.TestCase):
   def test_infers_breach_as_per_limits(self):
     self.assertTrue(typewise_alert.infer_breach(20, 50, 100) == 'TOO_LOW')
+    
   def test_infer_breach_for_too_high(self):
     self.assertTrue(typewise_alert.infer_breach(200, 50, 100) == 'TOO_HIGH')
+    
   def test_infer_breach_for_normal(self):
     self.assertTrue(typewise_alert.infer_breach(80, 50, 100) == 'NORMAL')#increased coverage from 36 to 41%
+    
+  def test_classify_temperature(self):
+     self.assertTrue(typewise_alert.classify_temperature_breach('PASSIVE_COOLING', -2)== 'TOO_LOW')
+    
    
  
 
