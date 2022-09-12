631b5ddad19dc3f60ce84edebe679f9abc346c25
manorama02 <53304326+manorama02@users.noreply.github.com>
2022-09-12 09:57:20 +0530

    Update string_calculator.py

--- string_calculator.py
+++ string_calculator.py
@@ -1,3 +1,8 @@
 def add(given_string):
     if given_string == "":
         return 0
+    numbers_list = given_string.split(",")
+    if len(numbers_list) == 1:
+        return int(numbers_list[0])
+    elif len(numbers_list) == 2:
+        return int(numbers_list[0]) + int(numbers_list[1])
